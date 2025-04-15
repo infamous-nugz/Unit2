859a5283ff691c6fbc7c6ad410ac4005daa471ed
infamous_nugz <rosarioj6@roverkids.org>
2025-04-15 14:02:07 -0400

    comp 2.2

---
+++ AnimalStampede/AnimalStampede/Assets/Course Library/SCRIPTS/destory.cs
@@ -0,0 +1,31 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class destory : MonoBehaviour
+
+{
+    private float topbound = 30;
+    private float downbound = -15;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topbound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < downbound)
+        {
+            Destroy(gameObject);
+        }
+        
+        
+    }
+}
