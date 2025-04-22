ada1cdd24143805e615eb424a395a7653274655f
infamous_nugz <rosarioj6@roverkids.org>
2025-04-22 14:08:09 -0400

    wip 2.3 ;)

--- AnimalStampede/AnimalStampede/Assets/Course Library/SCRIPTS/spawnmanger.cs
+++ AnimalStampede/AnimalStampede/Assets/Course Library/SCRIPTS/spawnmanger.cs
@@ -4,9 +4,8 @@ public class spawnmanger : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
 
-    public int animalIndex;
-
-
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,7 +18,11 @@ public class spawnmanger : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.S))
 
         {
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+            Instantiate(animalPrefabs[animalIndex], spawnPos ,animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
